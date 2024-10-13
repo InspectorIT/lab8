d06804319a3b8f1bd8e7f911622558475924671e
Степан Левин <sshipunovv008@gmail.com>
2024-10-13 21:02:04 +0300

    Ваше сообщение

--- hello.pas
+++ hello.pas
@@ -1,3 +1,9 @@
-﻿begin
-  Println('Привет, мир!');
-end.
+﻿##
+var clock := ReadInteger('Который час?');
+case clock of
+  4..10: Println('Доброе утро, мир!');
+  11..16: Println('Добрый день, мир!');
+  17..22: Println('Добрый вечер, мир!');
+  else
+    Println('Доброй ночи, мир!');
+end;
