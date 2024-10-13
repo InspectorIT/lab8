99209e063e72e3ff35f7501c5a6f699d38df4cf0
Степан Левин <sshipunovv008@gmail.com>
2024-10-13 21:17:07 +0300

    Ваше сообщение

---
+++ time.pas
@@ -0,0 +1,3 @@
+﻿##
+var yearIsLeap := ReadInteger('Введите год, а я определю, високосный он или нет:');
+Println($'Является ли год високосным? Ответ: {yearIsLeap.Divs(4) and (yearIsLeap.NotDivs(100) or yearIsLeap.Divs(400))}');
