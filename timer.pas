c20091d2f454bfa9e13197418110a9b6cb36d2cc
Степан Левин <sshipunovv008@gmail.com>
2024-10-13 21:59:06 +0300

    Программа, которая возвращает суммарное количество секунд по введённым с клавиатуры количеству часов, минут и секунд

---
+++ timer.pas
@@ -0,0 +1,7 @@
+﻿##
+var (hours, minutes, seconds) := ReadInteger3('Введите количество часов, минут и секунд:');
+Assert((hours > 0) and (minutes > 0) and (seconds > 0));
+hours *= 3600;
+minutes *= 60;
+seconds += hours + minutes;
+Println($'Cуммарное количество секунд: {seconds}');
